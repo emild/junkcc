4292e37225437e077689a60e44b3f25763ba7385
Emil D. Kohn <emilkohn@gmail.com>
2026-04-24 15:15:01 +0300

    Refactoring fix: Loop labeling should happen after break classification

--- src/compiler/parser/semantic_analyzer/labels.rs
+++ src/compiler/parser/semantic_analyzer/labels.rs
@@ -5,8 +5,8 @@ use super::super::ast::*;
 pub fn label_program(prog: &mut Program) -> Result<(), String>
 {
     super::goto_labels::resolve_program_goto_labels(prog)?;
-    super::loop_labeling::label_program_loops(prog)?;
     super::break_classifier::check_and_classify_program_break_statements(prog)?;
+    super::loop_labeling::label_program_loops(prog)?;
     super::switch_labeling::label_program_switch_statements(prog)?;
 
     Ok(())
